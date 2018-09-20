2df784e38e8d8e4940477763715341d21642e3cc
benslamahassen <benslamahassen5@gmail.com>
2018-09-20 22:01:27 +0100

    Fix get data after render() in Movie Page

--- src/pages/movie.js
+++ src/pages/movie.js
@@ -8,22 +8,36 @@ const apiEndpoint = config.apiEndpoint
 const apiToken = config.accessToken
 
 class Movie extends React.Component {
-  componentDidMount() {
+  state = { fetched: false }
+  fetchMovie() {
+    console.log('hee')
     let movieUid = new URL(this.props.location.href).searchParams.get('uid')
     Prismic.getApi(apiEndpoint, { accessToken: apiToken })
       .then(api => {
         api.getByUID('movie', movieUid).then(res => {
-          console.log(res)
+          this.setState({ movie: res.data, fetched: true })
         })
       })
       .catch(err => console.log(err))
   }
   render() {
-    return (
-      <Layout>
-        {new URL(this.props.location.href).searchParams.get('uid')}
-      </Layout>
-    )
+    if (!this.state.fetched && this.props.location.href) this.fetchMovie()
+    const movie = this.state.movie
+    let movieData = 'Loding...'
+    if (movie) {
+      movieData = (
+        <div className="row p-3">
+          <div className="col-6">
+            <img src={movie.poster.url} alt="" />
+          </div>
+          <div className="col-6">
+            <h3>{movie.title[0].text}</h3>
+            <p>{movie.description[0].text}</p>
+          </div>
+        </div>
+      )
+    }
+    return <Layout>{movieData}</Layout>
   }
 }
 
