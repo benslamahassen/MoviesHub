1e49897007bea06ef718e61e2a6c66cc817a33ee
benslamahassen <benslamahassen5@gmail.com>
2018-09-20 22:00:14 +0100

    New MovieCard Component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const MovieCard = props => {
+  return (
+    <Link key={props.index} to={'/movie?uid=' + props.uid}>
+      <div className="card shadow m-4 mx-3" style={{ width: '18rem' }}>
+        <img className="card-img-top" src={props.poster} alt="Movie" />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text text-truncate">{props.description}</p>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default MovieCard
